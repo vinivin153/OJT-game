1924274a1e3b0b1e2d0c00c1afb464b98ff6d647
vinivin153 <vinivin153@naver.com>
2025-06-18 11:40:30 +0900

    refactor: 메뉴 background 이미지 로딩을 preloader로 이동

--- src/game/scenes/Boot.ts
+++ src/game/scenes/Boot.ts
@@ -1,18 +1,13 @@
-import { Scene } from "phaser";
+import { Scene } from 'phaser';
 
 export class Boot extends Scene {
   constructor() {
-    super("Boot");
+    super('Boot');
   }
 
-  preload() {
-    //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
-    //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
-
-    this.load.image("menu-bg", "assets/menu_bg.png");
-  }
+  preload() {}
 
   create() {
-    this.scene.start("Preloader");
+    this.scene.start('Preloader');
   }
 }
